4bbed44fcbdf003a4a93474d984c63ead9fbab2d
Taranjot Singh <65440079+Taran05@users.noreply.github.com>
2021-04-05 09:16:17 +0530

    Update first.java

--- first.java
+++ first.java
@@ -7,12 +7,14 @@ Output: return num1+num2*/
 public class Main {
     public static void main(String[] args) {
         String s="3,1,6,4,2,3,7,2";
+        // converting the string into a string array that does not have "," and then converting that into List<String>
         List<String> numbers = Arrays.asList(s.split(","));
        int index_4=numbers.indexOf("4");
        int index_7=numbers.indexOf("7");
         int num1=0;
         for(int i=0;i<numbers.size();i++){
             if(i<index_4 || i>index_7){
+                //Integer.parseInt() for converting String into Integer
                 num1+=Integer.parseInt(numbers.get(i));
             }
         }
@@ -20,6 +22,7 @@ public class Main {
        for(int i=index_4;i<=index_7;i++){
            num2+=numbers.get(i);
        }
+        //Integer.parseInt() for converting String into Integer
         System.out.print(num1+Integer.parseInt(num2));
     }
 }
