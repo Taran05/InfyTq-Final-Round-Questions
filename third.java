8dccb77b26846fb9b51dd5a2ddf2c3cd9c248ebb
Taranjot Singh <65440079+Taran05@users.noreply.github.com>
2021-04-05 13:08:05 +0530

    Create third.java

---
+++ third.java
@@ -0,0 +1,22 @@
+Seperate odd place integers in a string. You have to return a 4-digit OTP by squarring the digits.
+  
+  
+  public class Main {
+    public static void main(String[] args) {
+       String s="5624381275";
+       String odd="";
+        
+        int i=0;
+       for(String e:s.split("")){
+           if(i%2!=0){
+               odd+=Math.round(Math.pow(Integer.parseInt(e),2));
+           }
+           if(odd.length()>=4){
+               break;
+           }
+           i++;
+      }
+       
+       
+    }
+}
