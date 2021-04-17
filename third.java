6ae5dfa32a26e88b001edca5aa2b1c31fbecb8a4
Taranjot Singh <65440079+Taran05@users.noreply.github.com>
2021-04-17 09:28:31 +0530

    Update third.java

--- third.java
+++ third.java
@@ -1,22 +1,19 @@
 /*Seperate odd place integers in a string. You have to return a 4-digit OTP by squarring the digits.*/
   
   
-  public class Main {
+ public class Main {
     public static void main(String[] args) {
-       String s="5624381275";
-       String odd="";
-        
-        int i=0;
-       for(String e:s.split("")){
-           if(i%2!=0){
-               odd+=Math.round(Math.pow(Integer.parseInt(e),2));
-           }
-           if(odd.length()>=4){
-               break;
-           }
-           i++;
-      }
-       
-       
+       String s="5621381275";
+        String temp[] = s.split("");
+        String ans="";
+        for(int i=0;i<temp.length;i++){
+            if(i%2!=0){
+                ans+=String.valueOf((int)Math.pow(Integer.parseInt(temp[i]),2));
+            }
+            if(ans.length()>=4){
+                break;
+            }
+        }
+        System.out.print(ans.substring(0,4));
     }
 }
